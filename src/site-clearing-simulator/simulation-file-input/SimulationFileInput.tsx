8e9c5574fae24215f9ad9acea144e8fd5bbbae31
Aleksandr Brenchev <abrenchev@griddynamics.com>
2021-02-04 21:27:05 +0100

    Implemented site map file upload

--- src/site-clearing-simulator/simulation-file-input/SimulationFileInput.tsx
+++ src/site-clearing-simulator/simulation-file-input/SimulationFileInput.tsx
@@ -1,28 +1,44 @@
-import { Card, Button } from '@material-ui/core'
-import React from 'react';
+import { Card, Typography, CardContent } from '@material-ui/core'
+import React, { useRef } from 'react';
 import { useDispatch } from 'react-redux';
 import { startSimulation } from '../store-slice';
-
-const dummyMap = [
-  ["o", "o", "t", "o", "o", "o", "o", "o", "o", "o"],
-  ["o", "o", "o", "o", "o", "o", "o", "T", "o", "o"],
-  ["r", "r", "r", "o", "o", "o", "t", "T", "o", "o"],
-  ["r", "r", "r", "r", "o", "o", "o", "o", "o", "o"],
-  ["r", "r", "r", "r", "r", "t", "o", "o", "o", "o"]
-];
+import styles from './styles.module.css';
 
 export const SimulationFileInput = () => {
   const dispatch = useDispatch();
+  const fileFieldRef = useRef<HTMLInputElement>(null);
 
-  const handleStartButtonClick = () => {
-    dispatch(startSimulation(dummyMap));
+  const handleFileUpload = async () => {
+    const fileField = fileFieldRef.current;
+    if (fileField === null) {
+      return;
+    }
+
+    const uploadedFiles = fileField.files;
+    if (uploadedFiles != null) {
+      const mapFile = uploadedFiles[0];
+      const textContent = await mapFile.text();
+
+      const siteRows = textContent.split('\n');
+      const siteMap = [
+        ...siteRows.map((row) => row.split(''))
+      ];
+
+      dispatch(startSimulation(siteMap));
+    }
   };
 
   return (
     <Card>
-      <Button onClick={handleStartButtonClick}>
-        Start simulation
-      </Button>
+      <CardContent>
+        <form>
+          <Typography className={styles.fileFieldLabel} component="label" htmlFor="simulationFile" gutterBottom>
+            Please, provide a file to start simulation:
+          </Typography>
+
+          <input ref={fileFieldRef} id="simulationFile" type="file" accept="text/plain" onChange={handleFileUpload}/>
+        </form>
+      </CardContent>
     </Card>
   );
 };
